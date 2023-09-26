86f30cdb77e781283726baa0f899d2381c57bbb6
apokrif6 <j.berenchan@gmail.com>
2023-09-26 22:33:17 +0200

    Create basic http server and add dockerfile

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"Manabu/service"
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", GetRandomFlashcard)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Listen and serve error:", err)
+	}
+}
+
+func GetRandomFlashcard(w http.ResponseWriter, r *http.Request) {
 	kanji := service.GetRandomKanji(1)
 
 	word := service.GetRandomWordByKanji(kanji)
 
-	fmt.Println("Kanji:", word.Kanji)
-	fmt.Println("Glosses:", word.Meanings[0].Glosses[0])
-	fmt.Println("Pronounced:", word.Variants[0].Pronounced)
-	fmt.Println("Written:", word.Variants[0].Written)
+	fmt.Fprintf(w, fmt.Sprintf("Kanji: %s \nGlosses: %s \nPronounced %s \nWritten: %s",
+		word.Kanji, word.Meanings[0].Glosses[0], word.Variants[0].Pronounced, word.Variants[0].Written))
 }
