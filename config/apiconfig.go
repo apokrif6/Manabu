e30ab5d0f15c10c654e01e35db9d978431572260
apokrif6 <j.berenchan@gmail.com>
2023-10-03 21:58:46 +0200

    Replace var with const in config

--- config/apiconfig.go
+++ config/apiconfig.go
@@ -1,8 +1,8 @@
 package apiconfig
 
-var kanjiGradeEndpoint = "https://kanjiapi.dev/v1/kanji/grade-"
+const kanjiGradeEndpoint = "https://kanjiapi.dev/v1/kanji/grade-"
 
-var kanjiWordsEndpoint = "https://kanjiapi.dev/v1/words/"
+const kanjiWordsEndpoint = "https://kanjiapi.dev/v1/words/"
 
 func KanjiGradeEndpoint() string {
 	return kanjiGradeEndpoint
